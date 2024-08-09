64fe3c86e739ed075053c1dff23b15c5f93f1fc9
Srihari R <120409108+srihari-976@users.noreply.github.com>
2024-08-09 23:29:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,5 +42,3 @@ def qa():
     data = {"result": "Thank you! I'm just a machine learning model designed to respond to questions and generate text based on my training data. Is there anything specific you'd like to ask or discuss? "}
 
     return jsonify(data)
-
-app.run(debug=True)
